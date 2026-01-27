7a164d4a1261f774f3b9e1812e5c88fe56fd8c60
Olena Kysilova <e.a.kiseljova@gmail.com>
2026-01-27 11:43:30 +0200

    Add Expense CRUD routes (draft)

--- app.py
+++ app.py
@@ -6,5 +6,25 @@ app = Flask(__name__)
 def home():
     return jsonify(message="Hello, I'm your Expense tracking App!")
 
+@app.route("/expense", methods=['POST'])
+def create_expense():
+    pass
+
+@app.route("/expense", methods=['GET'])
+def get_expenses():
+    pass
+
+@app.route("/expense/<int:id>", methods=['GET'])
+def get_expense(id: int):
+    pass
+
+@app.route("/expense/<int:id>", methods=['PATCH'])
+def update_expense(id: int):
+    pass
+
+@app.route("/expense/<int:id>", methods=['DELETE'])
+def delete_expense(id: int):
+    pass
+
 if __name__ == "__main__":
     app.run(debug=True)
