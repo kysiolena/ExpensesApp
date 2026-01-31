b0bcb259180176b9d9769c6f3d3e9a7513bd36eb
Olena Kysilova <e.a.kiseljova@gmail.com>
2026-01-31 15:16:17 +0200

    Add created_at and updated_at indexes'

--- app/models.py
+++ app/models.py
@@ -1,4 +1,5 @@
 from datetime import datetime, timezone
+
 from sqlalchemy.orm import Mapped, mapped_column
 
 from app.db import db
@@ -7,9 +8,10 @@ from app.db import db
 class TimestampModel(db.Model):
     __abstract__ = True
     created_at: Mapped[datetime] = mapped_column(
-        default=lambda: datetime.now(timezone.utc)
+        default=lambda: datetime.now(timezone.utc), index=True
     )
     updated_at: Mapped[datetime] = mapped_column(
         default=lambda: datetime.now(timezone.utc),
         onupdate=lambda: datetime.now(timezone.utc),
+        index=True,
     )
